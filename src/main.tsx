353f43a53823304c44554351f9340134d7f92c80
pixel-apprentice <jguiditta@gmail.com>
2026-03-17 08:18:03 -0400

    fix: harden Chromecast Ultra stability by bypassing Service Worker

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,20 @@ import App from './App.tsx';
 import './index.css';
 import { registerSW } from 'virtual:pwa-register';
 
-const updateSW = registerSW({
-  onNeedRefresh() {
-    window.dispatchEvent(new CustomEvent('fmn:pwa-update-available', { detail: updateSW }));
-    console.log('[PWA] New content available.');
-  },
-  onOfflineReady() {
-    console.log('[PWA] App is ready to work offline!');
-  },
-});
+const isTV = typeof window !== 'undefined' && window.navigator.userAgent.indexOf('CrKey') > -1;
+
+let updateSW: any;
+if (!isTV) {
+  updateSW = registerSW({
+    onNeedRefresh() {
+      window.dispatchEvent(new CustomEvent('fmn:pwa-update-available', { detail: updateSW }));
+      console.log('[PWA] New content available.');
+    },
+    onOfflineReady() {
+      console.log('[PWA] App is ready to work offline!');
+    },
+  });
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
